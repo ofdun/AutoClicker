bbd5609e6eb63f1a6a2542157d1c258095a345c1
ofdun <elik394@mail.ru>
2023-07-07 23:43:50 +0300

    saving settings to config

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PyQt5.QtGui import QKeySequence
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton,
                             QLabel, QHBoxLayout, QWidget, QLineEdit,
                             QVBoxLayout)
+import configparser
 
 EXCEPTION_KEYS = {
             16777248: 'SHIFT',
@@ -42,7 +43,8 @@ class MainWindow(QMainWindow):
         press_label.adjustSize()
         press_label.setMaximumWidth(50)
         
-        self.button_to_click = QPushButton("btn")
+        self.button_to_click = QPushButton('')
+        self.button_to_click.last = ''
         self.button_to_click.adjustSize()
         self.button_to_click.setMaximumWidth(150)
         self.button_to_click.clicked.connect(self.choose_button_to_click)
@@ -51,14 +53,14 @@ class MainWindow(QMainWindow):
         every_label = QLabel("every")
         every_label.adjustSize()
         
-        every_time_click = QLineEdit('1000')
-        every_time_click.adjustSize()
+        self.every_time_click = QLineEdit('')
+        self.every_time_click.adjustSize()
 
         mills_for_label = QLabel("mills for")
         mills_for_label.adjustSize()
 
-        click_time = QLineEdit('0')
-        click_time.adjustSize()
+        self.click_time = QLineEdit('')
+        self.click_time.adjustSize()
 
         seconds_label = QLabel('seconds.')
         seconds_label.adjustSize()
@@ -67,12 +69,17 @@ class MainWindow(QMainWindow):
         hbox1.addWidget(press_label)
         hbox1.addWidget(self.button_to_click)
         hbox1.addWidget(every_label)
-        hbox1.addWidget(every_time_click)
+        hbox1.addWidget(self.every_time_click)
         hbox1.addWidget(mills_for_label)
-        hbox1.addWidget(click_time)
+        hbox1.addWidget(self.click_time)
         hbox1.addWidget(seconds_label)
 
+        reset_button = QPushButton("Reset Config")
+        reset_button.adjustSize()
+        reset_button.clicked.connect(self.reset_config)
+
         hbox2 = QHBoxLayout()
+        hbox2.addWidget(reset_button)
 
         vbox = QVBoxLayout()
         vbox.addLayout(hbox1)
@@ -81,7 +88,8 @@ class MainWindow(QMainWindow):
         central_widget = QWidget()
         central_widget.setLayout(vbox)
         self.setCentralWidget(central_widget)
-        
+        self.render_config_updates()
+
         self.show()
 
     @staticmethod
@@ -90,6 +98,33 @@ class MainWindow(QMainWindow):
             if val == value:
                 return key
         raise ValueError("Value not found in dictionary")
+    
+    @staticmethod
+    def load_config() -> configparser.ConfigParser:
+        cfg = configparser.ConfigParser()
+        cfg.read_file(open('config.cfg'))
+        return cfg
+    
+    def render_config_updates(self):
+        cfg = self.load_config()
+        default_button = cfg.get('DEFAULT', 'button_to_press')
+        click_every = cfg.get('DEFAULT', 'click_every')
+        for_seconds = cfg.get('DEFAULT', 'for_seconds')
+        self.button_to_click.setText(default_button)
+        self.button_to_click.last = default_button
+        self.every_time_click.setText(click_every)
+        self.click_time.setText(for_seconds)
+    
+    def reset_config(self):
+        cfg = self.load_config()
+        cfg['DEFAULT'] = {
+            'button_to_press': "LMB",
+            'click_every': "1000",
+            'for_seconds': "0"
+        }
+        with open('config.cfg', 'w') as configfile:
+            cfg.write(configfile)
+        self.render_config_updates()
 
     def choose_button_to_click(self) -> None:
         self.ready_to_change_key = True
@@ -107,7 +142,7 @@ class MainWindow(QMainWindow):
 
         if self.ready_to_change_key:
             self.button_to_click.setText(symbol)
-            self.button_to_click.last_pressed_button = key
+            self.button_to_click.last = symbol
             self.ready_to_change_key = False
     
     def on_mouse_key_pressed(self, event) -> None:
@@ -117,7 +152,6 @@ class MainWindow(QMainWindow):
 
         if self.ready_to_change_key:
             self.button_to_click.setText(symbol)
-            self.button_to_click.last_pressed_button = key
             self.ready_to_change_key = False
     
     def event(self,event):
@@ -126,6 +160,17 @@ class MainWindow(QMainWindow):
         elif event.type() == QEvent.MouseButtonPress:
             self.on_mouse_key_pressed(event)
         return QMainWindow.event(self, event)
+    
+    def closeEvent(self, a0) -> None:
+        cfg = self.load_config()
+        cfg['DEFAULT'] = {
+            'button_to_press': self.button_to_click.last,
+            'click_every': self.every_time_click.text(),
+            'for_seconds': self.click_time.text()
+        }
+        with open('config.cfg', 'w') as configfile:
+            cfg.write(configfile)
+        return super().closeEvent(a0)
 
 
 if __name__ == '__main__':
